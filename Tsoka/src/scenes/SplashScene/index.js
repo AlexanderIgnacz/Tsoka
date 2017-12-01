36578083e205020ff1239d661d3fca5d1c066628
Alexander Ignacz <alex.ignacz412@gmail.com>
2017-12-01 23:59:10 +0400

    useful codes

---
+++ Tsoka/src/scenes/SplashScene/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import styles from './styles';
+
+
+export default class SplashScene extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    );
+  }
+}
---
+++ Tsoka/src/scenes/SplashScene/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: 'gray',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    resizeMode: 'contain',
+    width: 100,
+  }
+});
